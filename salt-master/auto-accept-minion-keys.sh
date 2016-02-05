a4b63a0412d5eebd8315a1be9731f29c87e7e4e0
russellballestrini <russell.ballestrini@gmail.com>
2016-02-05 14:59:32 +0000

    awesome bash script for automation.
    	new file:   auto-accept-minion-keys.sh

---
+++ salt-master/auto-accept-minion-keys.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "./$0 <seconds-to-auto-accept>"
+    exit 1
+fi
+
+seconds=$1
+tmp_config="/etc/salt/master.d/auto-accept-minion-keys.conf"
+
+# register disable_auto_accept function when complete or aborting.
+trap disable_auto_accept 0 1 2 3 15
+
+function enable_auto_accept() {
+    echo "auto_accept: True" > $tmp_config
+    service salt-master restart
+    echo "enabled auto_accept of minion keys."  
+}
+
+function disable_auto_accept() {
+    rm $tmp_config
+    service salt-master restart
+    echo "disabled auto_accept of minion keys."  
+}
+
+enable_auto_accept
+echo "sleeping for $seconds ... please create new minions in another window."
+sleep $seconds
