d43c0d66d75e1fb391265ae86f5db6858b5b6588
Russell Ballestrini <russell.ballestrini@gmail.com>
2016-02-05 15:10:30 -0500

    Update bootstrap-salt-minion.sh

--- sdc/bootstrap-salt-minion.sh
+++ sdc/bootstrap-salt-minion.sh
@@ -7,7 +7,7 @@ hostname $HOSTNAME
 echo "$HOSTNAME" > /etc/hostname
 
 wget -O install_salt.sh https://bootstrap.saltstack.com
-sudo sh install_salt.sh
+sh install_salt.sh
 
 echo "$HOSTNAME" > /etc/salt/minion_id
 echo "master: $SALTMASTER" > /etc/salt/minion.d/custom.conf
