5d9b272e6858488b841209673fd473fff7fe689e
Russell Ballestrini <russell.ballestrini@gmail.com>
2016-02-05 14:51:31 -0500

    Update auto-accept-minion-keys.sh

--- salt-master/auto-accept-minion-keys.sh
+++ salt-master/auto-accept-minion-keys.sh
@@ -3,6 +3,8 @@
 # This script "unlocks the door" on the salt-master for a given amount of time
 # such that a fleet of new machines / minions can be brought into existance,
 # automatically accepted by the salt-master, and provisioned for their role.
+# 
+# This script will automatically "lock the door" on errors or when the time is up.
 
 if [ -z "$1" ]
   then
